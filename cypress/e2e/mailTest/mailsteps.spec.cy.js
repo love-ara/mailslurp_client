fa6a19fd29a0de1f45307328653d8cd5211f1d2a
Aramide <aramideotenaike@gmail.com>
2024-12-04 03:24:09 +0100

    commit signup

--- cypress/e2e/mailTest/mailsteps.spec.cy.js
+++ cypress/e2e/mailTest/mailsteps.spec.cy.js
@@ -12,4 +12,12 @@ describe('testing email token extraction', () => {
   it('click the signup for free button', ()=>{
     cy.get(data.signupForFreeButton).should('exist').click();
   })
+
+  it('signup as a talent', () => {
+    cy.get(data.signupAsATalentButton).eq(0).should('exist').click();
+  })
+  it('fill in the first name', ()=>{
+    cy.get(data.firstnameField).should('exist').type('Aramide');
+  })
+  
 })
